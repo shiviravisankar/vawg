fad3ebf86081936b1e87631c8aec826777494b46
shiviravisankar <shiviravis@gmail.com>
2024-07-15 16:29:35 -0700

    about page done

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,8 @@
+//pulse animation
 document.addEventListener('DOMContentLoaded', () => {
-    const circles = document.querySelectorAll('.circle');
-    
-    circles.forEach((circle, index) => {
-        circle.style.animation = `pulse 4s infinite ${index * 0.5}s`;
-    });
+const circles = document.querySelectorAll('.circle');
+
+circles.forEach((circle, index) => {
+circle.style.animation = `pulse 4s infinite ${index * 0.5}s`;
+});
 });
